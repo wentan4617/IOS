8b2d573b636ceb1693e1bad0b71c1231000b742b
went <wentan4617@gmail.com>
2016-05-21 18:30:57 +1200

    initialize a demo project

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by went_macbook on 16/5/21.
+//  Copyright © 2016年 went. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
