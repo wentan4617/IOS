8b085873adc7aa78ddbad9e34d593ad24dc8cd58
went <wentan4617@gmail.com>
2016-05-22 22:16:23 +1200

    modify directory

--- Demo/Demo/classes/main/AppDelegate.m
+++ Demo/Demo/classes/main/AppDelegate.m
@@ -7,6 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import "HomeTableViewController.h"
+#import "MessageTableViewController.h"
+#import "DiscoverTableViewController.h"
+#import "ProfileTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,8 +20,41 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    UITabBarController *tbController = [[UITabBarController alloc]init];
+    self.window.rootViewController = tbController;
+    HomeTableViewController *home = [[HomeTableViewController alloc]init];
+    [self addVc:home withTitle:@"home" withImage:@"tabbar_home" withSelectedImage:@"tabbar_home_highlighted"];
+    MessageTableViewController *message = [[MessageTableViewController alloc]init];
+    [self addVc:message withTitle:@"message" withImage:@"tabbar_message_center" withSelectedImage:@"tabbar_message_center_highlighted"];
+
+    DiscoverTableViewController *discover = [[DiscoverTableViewController alloc]init];
+    [self addVc:discover withTitle:@"discover" withImage:@"tabbar_discover" withSelectedImage:@"tabbar_discover_highlighted"];
+
+    ProfileTableViewController *profile = [[ProfileTableViewController alloc]init];
+    [self addVc:profile withTitle:@"profile" withImage:@"tabbar_profile" withSelectedImage:@"tabbar_profile_highlighted"];
+
+    
+    
+    
+    [self.window makeKeyAndVisible];
+    
+    
     return YES;
+}
+
+-(void)addVc:(UIViewController *)childController withTitle:(NSString *)title withImage:(NSString *)image withSelectedImage:(NSString *)selectedImage{
+    childController.tabBarItem.title = title;
+    childController.tabBarItem.image = [UIImage imageNamed:image];
+    childController.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImage] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    NSMutableDictionary *textDic = [[NSMutableDictionary alloc]init];
+    textDic[NSForegroundColorAttributeName] = RGBColor(123, 123, 123);
+    [childController.tabBarItem setTitleTextAttributes:textDic forState:UIControlStateNormal];
+    NSMutableDictionary *selectTextDIc = [[NSMutableDictionary alloc]init];
+    selectTextDIc[NSForegroundColorAttributeName] = [UIColor orangeColor];
+    [childController.tabBarItem setTitleTextAttributes:selectTextDIc forState:UIControlStateSelected];
+    [self.window.rootViewController addChildViewController:childController];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
