792d3a6971bc3f8f9f395af745264a439f1114cf
went <wentan4617@gmail.com>
2016-05-22 20:23:25 +1200

    test2

--- demo/demo/classes/main/AppDelegate.m
+++ demo/demo/classes/main/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = RandomColor;
     UITabBarController *tbController = [[UITabBarController alloc]init];
     self.window.rootViewController = tbController;
     [self.window makeKeyAndVisible];
